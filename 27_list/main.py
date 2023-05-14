3b37447e6f45a2ef6fd577b3f3e482137a9f968d
Ananthu PM <ananthupmadhu@gmail.com>
2023-05-14 13:54:11 +0530

    list

---
+++ 27_list/main.py
@@ -0,0 +1,13 @@
+colors = ["read","green","blue","yellow"]
+
+print(colors)
+print(colors[0])
+print(colors[1])
+print(colors[-1])
+print(colors[2])
+
+colors[1]="pink"
+print(colors[1])
+
+colors.append("red")
+print(colors[-1])
