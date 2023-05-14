f5d99987f3a98cbc2328516b7b224155f15858c9
Ananthu PM <ananthupmadhu@gmail.com>
2023-05-14 15:06:17 +0530

    fix play code

--- 29_play/main.py
+++ 29_play/main.py
@@ -3,10 +3,10 @@
 ROW1 = ["😀","😄","🤣"]
 ROW2 = ["😍","😉","😘"]
 ROW3 = ["😔","😕","🤨"]
-map = [ROW1,ROW2,ROW3]
+MAP = [ROW1,ROW2,ROW3]
 print(f"{ROW1}\n{ROW2}\n{ROW3}\n")
 position = input("Where do you want to put treasure? ")
 column = int(position[0])
 row = int(position[1])
-map[row-1][column-1]="🎁"
+MAP[row-1][column-1]="🎁"
 print(f"{ROW1}\n{ROW2}\n{ROW3}\n")
