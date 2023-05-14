cec12051fb7917b5f8c1c10f70ffa208ce2a265f
Ananthu PM <ananthupmadhu@gmail.com>
2023-05-14 15:51:41 +0530

    update game error condtion

--- 30_stone_scissors_paper/main.py
+++ 30_stone_scissors_paper/main.py
@@ -2,44 +2,45 @@
 import random
 
 ITEMS=["🪨","🔪","📄"]
-print("Welecome To Stone Scissors Paper")
+print("Welecome To Rock Scissors Paper")
 print("================================")
 COMPUTER_SCORE = 0
 USER_SCORE = 0
-USER_CHOICE="start"
-while USER_CHOICE != "e":
-    user_choice_str = input("Enter your choice 1-stone 2-scissors 3-paper e-exit :\t")
-    if user_choice_str != "e":
-        USER_CHOICE = int(user_choice_str)
-        COMPUTER_CHOICE = random.randint(1,3)
-        print(f"Your choice is {ITEMS[USER_CHOICE-1]}")
-        print(f"Computer choice is {ITEMS[COMPUTER_CHOICE-1]}")
-        if USER_CHOICE==1:
-            if COMPUTER_CHOICE == 1:
-                print("DRAW 💥")
-            elif COMPUTER_CHOICE == 2:
-                print("You Wone 😊")
-                USER_SCORE += 1
-            elif COMPUTER_CHOICE == 3:
-                print("Computer Wone 😒")
-                COMPUTER_SCORE += 1
-        if USER_CHOICE==2:
-            if COMPUTER_CHOICE == 1:
-                print("Computer Wone 😒")
-                COMPUTER_SCORE += 1
-            elif COMPUTER_CHOICE == 2:
-                print("DRAW 💥")
-            elif COMPUTER_CHOICE == 3:
-                print("You Wone 😊")
-                USER_SCORE += 1
-        if USER_CHOICE==3:
-            if COMPUTER_CHOICE == 1:
-                print("You Wone 😊")
-                USER_SCORE += 1
-            elif COMPUTER_CHOICE == 2:
-                print("Computer Wone 😒")
-                COMPUTER_SCORE += 1
-            elif COMPUTER_CHOICE == 3:
-                print("DRAW 💥")
-        print(f"Total Score Computer: {COMPUTER_SCORE}  Your: {USER_SCORE}")
+USER_CHOICE_STR="start"
+while USER_CHOICE_STR != "e":
+    USER_CHOICE_STR = input("Enter your choice 0-Rock 1-Scissors 2-Paper e-exit :\t")
+    if USER_CHOICE_STR != "e" and USER_CHOICE_STR.isdigit():
+        USER_CHOICE = int(USER_CHOICE_STR)
+        if USER_CHOICE in (0,1,2):
+            COMPUTER_CHOICE = random.randint(0,2)
+            print(f"Your choice is {ITEMS[USER_CHOICE]}")
+            print(f"Computer choice is {ITEMS[COMPUTER_CHOICE]}")
+            if USER_CHOICE==0:
+                if COMPUTER_CHOICE == 0:
+                    print("DRAW 💥")
+                elif COMPUTER_CHOICE == 1:
+                    print("You Wone 😊")
+                    USER_SCORE += 1
+                elif COMPUTER_CHOICE == 2:
+                    print("Computer Wone 😒")
+                    COMPUTER_SCORE += 1
+            if USER_CHOICE==1:
+                if COMPUTER_CHOICE == 0:
+                    print("Computer Wone 😒")
+                    COMPUTER_SCORE += 1
+                elif COMPUTER_CHOICE == 1:
+                    print("DRAW 💥")
+                elif COMPUTER_CHOICE == 2:
+                    print("You Wone 😊")
+                    USER_SCORE += 1
+            if USER_CHOICE==2:
+                if COMPUTER_CHOICE == 0:
+                    print("You Wone 😊")
+                    USER_SCORE += 1
+                elif COMPUTER_CHOICE == 1:
+                    print("Computer Wone 😒")
+                    COMPUTER_SCORE += 1
+                elif COMPUTER_CHOICE == 2:
+                    print("DRAW 💥")
+            print(f"Total Score Computer: {COMPUTER_SCORE}  Your: {USER_SCORE}")
 print("GAME OVER!!!")
