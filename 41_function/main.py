b45f0d5ae77812dd920aecf47fa15aee438ad9e8
Ananthu PM <ananthupmadhu@gmail.com>
2023-06-10 01:57:28 +0530

    add function

---
+++ 41_function/main.py
@@ -0,0 +1,19 @@
+"""Function example"""
+
+def greet():
+    """
+    Function without parameter
+    """
+    print(f"Hi,")
+    print("hope doing good\n")
+
+def greet_with_name(name):
+    """
+    Function with parameter
+    """
+    print(f"Hi {name},")
+    print("hope doing good\n")
+    
+if __name__=="__main__":
+    greet()
+    greet_with_name(name="Ananthu")
