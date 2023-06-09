818866284ad33417227f5c0706fab595afd282f7
Ananthu PM <ananthupmadhu@gmail.com>
2023-06-10 02:04:21 +0530

    add function with multiple arg

--- 41_function/main.py
+++ 41_function/main.py
@@ -13,7 +13,15 @@ def greet_with_name(name):
     """
     print(f"Hi {name},")
     print("hope doing good\n")
+    
+def greet_with_name_and_location(name,location):
+    """
+    with multiple args
+    """
+    print(f"Hi {name},")
+    print(f"How is wether in {location}\n")
 
 if __name__=="__main__":
     greet()
     greet_with_name(name="Ananthu")
+    greet_with_name_and_location(name="Ananthu",location="Kattappana")
