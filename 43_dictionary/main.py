97914744edd1dc2d5f9596bae26bc05af5f67298
Ananthu PM <ananthupmadhu@gmail.com>
2023-06-11 14:05:15 +0530

    add dictionary pgm

---
+++ 43_dictionary/main.py
@@ -0,0 +1,6 @@
+"""dictionary program"""
+
+SAMPLE ={"first_name":"Ananthu","last_name":"PM"}
+SAMPLE["dob"]="16-May-1995"
+
+print(f'Name: {SAMPLE["first_name"]} {SAMPLE["last_name"]}, Dob: {SAMPLE["dob"]}')
